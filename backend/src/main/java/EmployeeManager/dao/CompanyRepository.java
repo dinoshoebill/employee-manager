96b6538e1992275b0ea2758f3523ef756983370f
dalen.grdic <dalen.grdic@fer.hr>
2023-03-02 14:32:31 +0200

    company repo

---
+++ backend/src/main/java/EmployeeManager/dao/CompanyRepository.java
@@ -0,0 +1,16 @@
+package EmployeeManager.dao;
+
+import EmployeeManager.models.Company;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+@Repository
+public interface CompanyRepository extends JpaRepository<Company, UUID> {
+
+    Optional<Company> findByCompanyId(UUID companyId);
+
+    Boolean existsByName(String Name);
+}
--- backend/src/main/java/EmployeeManager/dao/EmployeeRepository.java
+++ backend/src/main/java/EmployeeManager/dao/EmployeeRepository.java
@@ -1,7 +1,6 @@
 package EmployeeManager.dao;
 
 import EmployeeManager.models.Employee;
-import EmployeeManager.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
