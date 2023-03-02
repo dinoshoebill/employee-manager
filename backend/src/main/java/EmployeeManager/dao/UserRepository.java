7eb2cd1e40c3e96d5fc97849bbb8c17d98ee2de0
dalen.grdic <dalen.grdic@fer.hr>
2023-03-02 14:11:57 +0200

    user repo

---
+++ backend/src/main/java/EmployeeManager/dao/UserRepository.java
@@ -0,0 +1,20 @@
+package EmployeeManager.dao;
+
+import EmployeeManager.models.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
+
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+
+    Optional<User> findById(Long id);
+
+    Boolean existsByUsername(String username);
+
+    Boolean existsByEmail(String email);
+}
