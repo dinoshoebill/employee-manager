b871537b151ba25bbe36f2256a69b477ed63b344
dalen.grdic <dalen.grdic@fer.hr>
2023-03-02 12:01:25 +0200

    user interface

---
+++ backend/src/main/java/EmployeeManager/services/intf/IUserService.java
@@ -0,0 +1,21 @@
+package EmployeeManager.services.intf;
+
+import EmployeeManager.models.User;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface IUserService {
+
+    List<User> listAllUsers();
+
+    Optional<User> findByIdentificator(Long identificator)
+
+    Optional<User> findById(Long userId);
+
+    User createNewUser(User user);
+
+    User updateUser(User user);
+
+    User deleteUser(Long userId);
+}
