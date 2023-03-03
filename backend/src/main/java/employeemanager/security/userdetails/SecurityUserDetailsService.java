2eb469e8fdc8df8b3e5497c8f2c64743ce68cd24
dalen.grdic <dalen.grdic@fer.hr>
2023-03-03 14:39:41 +0200

    user details service

---
+++ backend/src/main/java/employeemanager/security/userdetails/SecurityUserDetailsService.java
@@ -0,0 +1,25 @@
+package employeemanager.security.userdetails;
+
+import employeemanager.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class SecurityUserDetailsService implements UserDetailsService {
+    @Autowired
+    UserRepository userRepo;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(email)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with email: " + email));
+
+        return SecurityUserDetails.buildUser(user);
+    }
+
+}
