34e0260088c6baa3cbb2b4a1a7eaf00000e36a35
dalen.grdic <dalen.grdic@fer.hr>
2023-03-05 12:40:27 +0200

    test controller

---
+++ backend/src/main/java/employeemanager/controllers/TestController.java
@@ -0,0 +1,30 @@
+package employeemanager.controllers;
+
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/api/test")
+public class TestController {
+
+    @GetMapping("/all")
+    public String allAccess() {
+        return "Public content route.";
+    }
+    
+    @GetMapping("/employee_admin")
+    @PreAuthorize("hasAuthority('EMPLOYEE_ADMIN')")
+    public String employeeAdminAccess() {
+        return "Employee admin content.";
+    }
+
+    @GetMapping("/app_admin")
+    @PreAuthorize("hasAuthority('APP_ADMIN')")
+    public String appAdminAccess() {
+        return "App admin content.";
+    }
+}
