4290a9630cd7c497e1ecc312e406fdf20bea8f16
dalen.grdic <dalen.grdic@fer.hr>
2023-03-05 12:57:00 +0200

    signup request

---
+++ backend/src/main/java/employeemanager/security/payload/request/SignupRequest.java
@@ -0,0 +1,28 @@
+package employeemanager.security.payload.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public class SignupRequest {
+
+    @NotBlank
+    @Size(max = 50)
+    private String name;
+
+    @NotBlank
+    @Size(max = 50)
+    private String surname;
+
+    @NotBlank
+    @Size(max = 70)
+    @Email
+    private String email;
+
+    @NotBlank
+    private String role;
+
+    @NotBlank
+    @Size(min = 10, max = 30)
+    private String password;
+}
